10ae1c0d4675ab20950f8e6e6ade8cc9b7892244
André Ezías <andre.ezias@webhub.com.br>
2020-07-27 14:53:10 -0300

    Configurações inicias e estrutura básica de serviço.

--- packages/Ezias/MultiQueue/config/multiqueue.php
+++ packages/Ezias/MultiQueue/config/multiqueue.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //
+    "num_queue_process" => env("NUM_QUEUE_PROCS", 5),
+    "connection" => env('QUEUE_CONNECTION', 'sync')
 ];
--- packages/Ezias/MultiQueue/src/MultiQueue.php
+++ packages/Ezias/MultiQueue/src/MultiQueue.php
@@ -2,7 +2,56 @@
 
 namespace Ezias\MultiQueue;
 
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Queue;
+use Illuminate\Support\Facades\DB;
+
 class MultiQueue
 {
-    // Build wonderful things
+    public function getNextProcess()
+    {
+        $queues = $this->getQueueList();
+        $lastJob = $this->getLastJobQueued();
+        $nextQueue = "process_00";
+
+        if (is_null($lastJob)) {
+            return $nextQueue;
+        }
+
+        foreach ($queues as $key => $queue) {
+            if ($lastJob->queue === $queue) {
+                $nextQueue = $queues[$key+1] ?? $nextQueue;
+            }
+        }
+
+        return $nextQueue;
+    }
+
+    protected function getLastJobQueued()
+    {
+        $queueConnection = config("multiqueue.connaction");
+
+        if ($queueConnection == "database") {
+            return DB::table("jobs")->get()->last();
+        }
+
+        return Queue::after(function (JobProcessed $event) {
+            if (Queue::size($event->job->getQueue()) == 0) {
+                return $event->job->getQueue();
+            }
+        });
+    }
+
+    protected function getQueueList()
+    {
+        $procs = array_fill(0, config("multiqueue.num_process"), "process_");
+        $queues = [];
+
+        foreach ($procs as $key => $value) {
+            $zero = $key < 10 ? "0":"";
+            $queues[]=$value.$zero.$key;
+        }
+
+        return $queues;
+    }
 }
